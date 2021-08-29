75ae23e207fb7b8ad8e7f0a098d6a18c651ab95d
theansonia <theansonia@users.noreply.github.com>
2021-08-28 21:23:51 -0400

    read me

--- src/client/components/AntLoading.tsx
+++ src/client/components/AntLoading.tsx
@@ -1,24 +1,25 @@
 import { ReactComponent as AntPic } from './ant.svg';
+const width = window.innerWidth;
 
 export const AntLoading = (): JSX.Element => (
   <div id='ants'>
-    <AntPic style={{ width: '10vw', right: '135vw', top: '15vw' }} />
+    <AntPic style={{ width: '10vw', right: width * 1.5, top: '15vw' }} />
     <AntPic
-      style={{ width: '10vw', right: '120vw', fill: 'red', top: '15vw' }}
+      style={{ width: '10vw', right: width * 1.35, fill: 'red', top: '15vw' }}
     />
     <AntPic
       style={{
         width: '10vw',
-        right: '105vw',
+        right: width * 1.1,
         fill: 'silver',
         top: '15vw',
       }}
     />
     <AntPic
-      style={{ width: '10vw', right: '90vw', fill: 'black', top: '15vw' }}
+      style={{ width: '10vw', right: width * 0.95, fill: 'black', top: '15vw' }}
     />
     <AntPic
-      style={{ width: '10vw', right: '75vw', fill: 'red', top: '15vw' }}
+      style={{ width: '10vw', right: width * 0.8, fill: 'red', top: '15vw' }}
     />
   </div>
 );
